ed6636d8ed1a7f14d4570b46f54db059b69b5a59
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-06 19:32:56 +0800

    Code Complete - Fixed isdigit Error

--- MaxQuan_MR.py
+++ MaxQuan_MR.py
@@ -1,17 +1,28 @@
+import math 
+
 def user_inputs():
     while True:
         usr_balance = input("Enter your balance: ")
         cost = input("\nEnter the price of an apple: ")
-        if usr_balance.isdigit() and cost.isdigit() == True:
-            return usr_balance, cost
-        elif usr_balance.isdigit() or cost.isdigit() == True:
+        if usr_balance.replace(".", "", 1).isdigit() and cost.replace(".", "", 1).isdigit() == True:
+            money = float(usr_balance)
+            price = float(cost)
+            return money, price
+        elif usr_balance.replace(".", "", 1).isdigit() or cost.replace(".", "", 1).isdigit() == True:
             if usr_balance.isdigit() == False:
                 print("\nYour balance should be in numerical form.\n")
             else:
-                print("\nThe rpice of an apple should be in numerical form.\n")
+                print("\nThe price of an apple should be in numerical form.\n")
         else:
             print("\nPlease enter only numerical inputs.\n")
 
-fnl_output = user_inputs()
+def general_operator(balance, price):
+    max_no_apl = math.floor(balance/price) 
+    change = balance % price
+    return max_no_apl, change
 
-print(fnl_output)
+dividend, divisor = user_inputs()
+
+fnl_output1, fnl_output2 = general_operator(dividend, divisor)
+
+print(f"\nYou can buy {fnl_output1} apples and your change is {fnl_output2:.2f} pesos.")
