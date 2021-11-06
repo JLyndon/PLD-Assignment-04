f8bc3f83f279fc1d5ffc7a76d0df17eb9e41590b
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-06 20:01:58 +0800

    Merged Input_Instructions

--- TPrice_MR.py
+++ TPrice_MR.py
@@ -1,10 +1,7 @@
-def apl_quant():
+def ProductS_Quant():
     NumbApl = int(input("\nHow many apples would you like to buy? \n> "))
-    return NumbApl
-
-def orng_quant():
     NumbOrng = int(input("\nHow many oranges would you like to buy? \n> "))
-    return NumbOrng
+    return NumbApl, NumbOrng
 
 def total_(NumApl, NumOrng):
     ttlprc_apl = NumApl*20
@@ -12,6 +9,8 @@ def total_(NumApl, NumOrng):
     summation = ttlprc_apl + ttlprc_orng
     return summation
 
-grnd_total = total_(apl_quant(), orng_quant())
+apl_quant, orng_quant = ProductS_Quant()
+
+grnd_total = total_(apl_quant, orng_quant)
 
 print(f"\nThe total amount is {grnd_total}.")
