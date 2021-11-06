6fbf0a537df3d5d271ea8bdc814d7876e23577fa
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-06 18:27:10 +0800

    First Commit - Duplicates of Assignment 03

---
+++ MaxQuan_MR.py
@@ -0,0 +1,18 @@
+import math
+
+def user_money():
+    usr_balance = float(input("Enter your balance: "))
+    return usr_balance
+
+def apl_price():
+    cost = float(input("\nEnter the price of an apple: "))
+    return cost
+
+def general_operator(balance, price):
+    max_no_apl = math.floor(balance/price) 
+    change = balance % price
+    return max_no_apl, change
+
+fnl_output = general_operator(user_money(), apl_price())
+
+print(f"\nYou can buy {fnl_output[0]} apples and your change is {fnl_output[1]:.2f} pesos.")
---
+++ TPrice_MR.py
@@ -0,0 +1,17 @@
+def apl_quant():
+    NumbApl = int(input("\nHow many apples would you like to buy? \n> "))
+    return NumbApl
+
+def orng_quant():
+    NumbOrng = int(input("\nHow many oranges would you like to buy? \n> "))
+    return NumbOrng
+
+def total_(NumApl, NumOrng):
+    ttlprc_apl = NumApl*20
+    ttlprc_orng = NumOrng*25
+    summation = ttlprc_apl + ttlprc_orng
+    return summation
+
+grnd_total = total_(apl_quant(), orng_quant())
+
+print(f"\nThe total amount is {grnd_total}.")
