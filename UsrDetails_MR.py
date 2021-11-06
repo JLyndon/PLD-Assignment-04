4835af30d16b70af1f1e75ee899cbf0600dcb8b6
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-06 22:06:14 +0800

    Code Completed - Remove list => MultiReturnVal

--- UsrDetails_MR.py
+++ UsrDetails_MR.py
@@ -1,8 +1,8 @@
-def user_deets():
+def user_details():
     usr_name = input("\nEnter your name: ")
     usr_age = input("Enter age: ")
     usr_address = input("Enter your address: ") 
     return usr_name, usr_age, usr_address
 
-user_info = user_deets()
-print(f"\nHi, my name is {user_info[0]}. I am {user_info[1]} years old and I live in {user_info[2]}.")
+name, age, addr = user_details()
+print(f"\nHi, my name is {name}. I am {age} years old and I live in {addr}.")
