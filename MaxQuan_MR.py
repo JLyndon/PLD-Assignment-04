c355600260fc15086e7b4a193731ff005a5a4ed9
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-11-10 18:08:31 +0800

    Added Conditions for Inputs v01

--- MaxQuan_MR.py
+++ MaxQuan_MR.py
@@ -4,17 +4,19 @@ def user_inputs():
     while True:
         usr_balance = input("Enter your balance: ")
         cost = input("\nEnter the price of an apple: ")
-        if usr_balance.replace(".", "", 1).isdigit() and cost.replace(".", "", 1).isdigit() == True:
+        if usr_balance.isdigit() and cost.isdigit() == True:
             money = float(usr_balance)
             price = float(cost)
             return money, price
-        elif usr_balance.replace(".", "", 1).isdigit() or cost.replace(".", "", 1).isdigit() == True:
-            if usr_balance.isdigit() == False:
-                print("\nYour balance should be in numerical form.\n")
-            else:
-                print("\nThe price of an apple should be in numerical form.\n")
-        else:
-            print("\nPlease enter only numerical inputs.\n")
+        elif usr_balance.isspace() or cost.isspace() == True:
+            print("Please fill all the blanks.")
+        elif usr_balance.isalpha() and cost.isalpha() == True:
+                print("Inputs should be in numerical form.")
+        elif (usr_balance.isalpha() and cost.isalpha() == True) == False:
+            if usr_balance.isalpha() == True:
+                print("Your balance should be in numerical form.")
+            elif cost.isalpha() == True:
+                print("The price should be in numerical form.")           
 
 def general_operator(balance, price):
     max_no_apl = math.floor(balance/price) 
